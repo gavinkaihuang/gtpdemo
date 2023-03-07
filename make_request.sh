29db4d0cf0903a539f231f7b9f1fab2fff79954d
huangkai <huangkai@hujiang.com>
2023-03-07 14:32:13 +0800

    add: delete files

--- make_request.sh
+++
@@ -1,4 +0,0 @@
-curl https://api.openai.com/v1/completions \
--H "Content-Type: application/json" \
--H "Authorization: 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" \
--d '{"model": "text-davinci-003", "prompt": "Say this is a test", "temperature": 0, "max_tokens": 7}'
--- readorgaization.py
+++
@@ -1,5 +0,0 @@
-import os
-import openai
-openai.organization = "org-1etO7fMkXVo2glkZ9rrN6lsA"
-openai.api_key = "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"
-openai.Model.list()
