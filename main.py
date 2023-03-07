09337559fc23584107c25cf4e6c837d5d4d9bb44
huangkai <huangkai@hujiang.com>
2023-03-07 16:10:58 +0800

    update: debug

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == '__main__':
     openai.organization = "org-1etO7fMkXVo2glkZ9rrN6lsA"
     openai.api_key = os.getenv("OPENAI_API_KEY")
     # print(openai.api_key)
-    openai.Model.list() 
+    print(openai.Model.list())
