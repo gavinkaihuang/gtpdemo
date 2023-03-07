949aacbe9c4ee20cf90b8140490864ce2a14e31d
huangkai <huangkai@hujiang.com>
2023-03-07 14:57:16 +0800

    add: add main file

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import openai
+
+if __name__ == '__main__':
+    openai.organization = "org-1etO7fMkXVo2glkZ9rrN6lsA"
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    # print(openai.api_key)
+    openai.Model.list() 
